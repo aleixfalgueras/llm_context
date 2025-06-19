585223d938ba94a6efd9f8c6c55181cd6183e07c
Aleix Falgueras Casals <feina.aleix@gmail.com>
2025-06-19 15:29:40 +0300

    fix dates components

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
+      <html lang="en-GB" suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
