5c0a2346995acf4caedb6176430cfd8eeeb2825d
Aleix Falgueras Casals <feina.aleix@gmail.com>
2025-06-20 09:10:14 +0300

    better UX 3

--- components/diet-generator-dialog.tsx
+++ components/diet-generator-dialog.tsx
@@ -219,7 +219,7 @@ export function DietGeneratorDialog({ open, onOpenChange, clients }: DietGenerat
 
   return (
     <Dialog open={open} onOpenChange={handleClose}>
-      <DialogContent className="max-w-6xl h-[90vh] max-h-[90vh] flex flex-col overflow-hidden">
+      <DialogContent className="max-w-6xl max-h-[90vh] overflow-hidden flex flex-col">
         <DialogHeader>
           <DialogTitle className="flex items-center gap-2">
             <FileText className="h-5 w-5" />
--- components/workout-generator-dialog.tsx
+++ components/workout-generator-dialog.tsx
@@ -251,7 +251,7 @@ export function WorkoutGeneratorDialog({ open, onOpenChange, clients }: WorkoutG
 
   return (
     <Dialog open={open} onOpenChange={handleClose}>
-      <DialogContent className="max-w-6xl h-[90vh] max-h-[90vh] flex flex-col overflow-hidden">
+      <DialogContent className="max-w-6xl max-h-[90vh] overflow-hidden flex flex-col">
         <DialogHeader>
           <DialogTitle className="flex items-center gap-2">
             <Dumbbell className="h-5 w-5" />
