7c03bc9f03eeea15fc01a0cec63a4ff728f79dcc
Aleix Falgueras Casals <feina.aleix@gmail.com>
2025-07-05 14:45:53 +0300

    delete pre-commit and pre-push files

--- pre-push.ps1
+++
@@ -1,9 +0,0 @@
-param($localRef, $localSha, $remoteRef, $remoteSha)
-
-# Extract branch name from remote ref
-$branch = $remoteRef -replace 'refs/heads/', ''
-if ($branch -eq 'main' -or $branch -eq 'master') {
-  Write-Host "🚫 Blocking push to '$branch'. Use a feature branch instead."
-  exit 1
-}
-exit 0
