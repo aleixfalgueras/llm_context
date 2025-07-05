1817a4e0e62947083de3f2b8e8ac45711cf90331
Aleix Falgueras Casals <feina.aleix@gmail.com>
2025-07-05 14:45:53 +0300

    delete pre-commit and pre-push files

--- pre-commit.ps1
+++
@@ -1,5 +0,0 @@
-$branch = git rev-parse --abbrev-ref HEAD
-if ($branch -eq 'main' -or $branch -eq 'master') {
-  Write-Host "🚫 Commits on '$branch' are blocked. Checkout a feature branch."
-  [Environment]::Exit(1)
-}
