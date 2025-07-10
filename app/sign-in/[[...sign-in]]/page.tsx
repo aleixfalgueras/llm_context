2f1cb7cbca8e2b493eea714455465cd7cd69713b
Aleix Falgueras Casals <feina.aleix@gmail.com>
2025-07-10 09:31:50 +0300

    allow sign up from sign in

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -4,15 +4,25 @@ import Link from 'next/link'
 export default function Page() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 dark:bg-gray-900">
-      <SignIn />
+      <SignIn 
+        signUpUrl="/sign-up"
+        forceRedirectUrl="/"
+        appearance={{
+          elements: {
+            footerActionLink: "hidden"
+          }
+        }}
+      />
       <div className="mt-6 text-center text-sm text-gray-600 dark:text-gray-400">
-        <Link href="/terms" className="text-blue-600 dark:text-blue-400 hover:underline">
-          Terms of Service
-        </Link>
-        {' • '}
-        <Link href="/privacy" className="text-blue-600 dark:text-blue-400 hover:underline">
-          Privacy Policy
-        </Link>
+        <div>
+          <Link href="/terms" className="text-blue-600 dark:text-blue-400 hover:underline">
+            Terms of Service
+          </Link>
+          {' • '}
+          <Link href="/privacy" className="text-blue-600 dark:text-blue-400 hover:underline">
+            Privacy Policy
+          </Link>
+        </div>
       </div>
     </div>
   )
