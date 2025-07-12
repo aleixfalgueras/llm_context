2cc16555c3e006a753cae2e80bbefc2a71bbf53b
Aleix Falgueras Casals <feina.aleix@gmail.com>
2025-07-12 18:20:28 +0300

    fix chrome issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
   ],
   theme: {
   	extend: {
+  		screens: {
+  			'wide': '1400px',
+  		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
