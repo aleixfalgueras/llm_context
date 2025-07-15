cb73890ef8e78c3e517454072d1ef08baa57ad93
Aleix Falgueras Casals <feina.aleix@gmail.com>
2025-07-15 18:41:22 +0300

    fix admin prisma issues

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
+  adminPrisma: PrismaClient | undefined
 }
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
@@ -12,4 +13,16 @@ export const prisma = globalForPrisma.prisma ?? new PrismaClient({
   }
 })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma 
+export const adminPrisma = globalForPrisma.adminPrisma ?? new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DIRECT_URL
+    }
+  },
+  log: ['warn', 'error'],
+})
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+  globalForPrisma.adminPrisma = adminPrisma
+} 
