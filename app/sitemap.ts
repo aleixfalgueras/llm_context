306d55f0713421abc1d2cc8aad452e247b3bb4d5
Aleix Falgueras Casals <feina.aleix@gmail.com>
2025-09-28 17:05:11 +0300

    Added robots.ts and sitemap.ts.

---
+++ app/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://mia.community'
+
+  return [
+    {
+      url: baseUrl,
+      lastModified:  new Date('2025-09-28'),
+      changeFrequency: 'monthly',
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/terms`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/privacy`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+  ]
+}
